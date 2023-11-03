86a738ceac907c0ff15a8b7ec49cdb75d980c675
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-03 15:49:52 +0100

    added faker

---
+++ src/main/java/KlajdiNdoci/U5W1D4/MainRunner.java
@@ -0,0 +1,14 @@
+package KlajdiNdoci.U5W1D4;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MainRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
--- src/main/java/KlajdiNdoci/U5W1D4/entities/Utente.java
+++ src/main/java/KlajdiNdoci/U5W1D4/entities/Utente.java
@@ -1,6 +1,10 @@
 package KlajdiNdoci.U5W1D4.entities;
+import com.github.javafaker.Faker;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.Locale;
+
 @Entity
 @Table(name = "utenti")
 @Getter
@@ -15,4 +19,12 @@ public class Utente {
     private String username;
     private String nome_completo;
     private String email;
+
+    public static class UserBuilder{
+        private Faker faker = new Faker(Locale.ITALY);
+        private String username = faker.name().username();
+        private String nome_completo = faker.name().fullName();
+        private String email = faker.internet().emailAddress();
+
+    }
 }
